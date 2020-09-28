6b064e27bff149488f5f5d0671b511526b83975f
João Cavacos <cavacosdoesgaming@gmail.com>
2020-09-28 21:34:14 +0100

    More changes on main menu

--- MicroGame01/Assets/Scripts/ButtonEffect.cs
+++ MicroGame01/Assets/Scripts/ButtonEffect.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonEffect : MonoBehaviour
 {
-    public void ExitGame(){
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject optionsMenu;
+
+    void ExitGame(){
         Application.Quit();
         Debug.Log("The game has quitted");
+    }
+
+    void StartGame(){
+        SceneManager.LoadScene("Gameplay Scene");
+    }
+
+    void OpenOptions(){
+
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    void CloseOptions(){
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    void SaveOptions(){
+
     }
 
 }
