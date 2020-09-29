2dd3e0c086d256d21c33a3d3b5be4820dca9ca77
João Cavacos <cavacosdoesgaming@gmail.com>
2020-09-29 10:18:59 +0100

    Main menu finished
    Credits are going to be done in the end

--- MicroGame01/Assets/Scripts/ButtonEffect.cs
+++ MicroGame01/Assets/Scripts/ButtonEffect.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using TMPro;
 
 public class ButtonEffect : MonoBehaviour
 {
@@ -11,6 +12,31 @@ public class ButtonEffect : MonoBehaviour
     [SerializeField] GameObject optionsMenu;
 
     public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height){
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
 
     public void ExitGame(){
         Application.Quit();
@@ -30,19 +56,24 @@ public class ButtonEffect : MonoBehaviour
 
     }
 
+    public void setQuality(int qualityIndex){
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setResolution(int resIndex){
+
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void OpenOptions(){
 
         mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
     }
 
-    public void CloseOptions(){
+    public void SaveOptions(){
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
     }
-
-    public void SaveOptions(){
-
-    }
-
 }
